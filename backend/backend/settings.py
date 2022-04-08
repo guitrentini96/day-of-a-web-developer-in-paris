6252151dbbbdfb5cb80f0b5cab2fea3fb7e4aefc
guitrentini96 <guitrentini96@gmail.com>
2022-04-08 19:28:33 +0200

    migrated to a local postgreSQL database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DOAWDIP_DB',
+        'USER': 'postgres',
+        'PASSWORD': 'guit14021996',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
